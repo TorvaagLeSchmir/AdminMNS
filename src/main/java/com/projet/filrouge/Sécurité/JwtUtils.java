fe8dd9b4a8bcce3a6d9b8e59c19f6d0a38570afd
Torvaag <tfoinont@gmail.com>
2023-07-17 14:28:23 +0200

    ok

--- src/main/java/com/projet/filrouge/Sécurité/JwtUtils.java
+++ src/main/java/com/projet/filrouge/Sécurité/JwtUtils.java
@@ -7,26 +7,38 @@ import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
+
 @Service
 public class JwtUtils {
     public String generateJwt(UserDetails userDetails){
+        Date now = new Date();
+        Date expiryDate = new Date(now.getTime() + 3600000);
 
         return Jwts.builder()
                 .setSubject(userDetails.getUsername())
+                .setIssuedAt(now)
+                .setExpiration(expiryDate)
                 .signWith(SignatureAlgorithm.HS256, "lepetitbidoubiwap")
                 .compact();
     }
 
-    public Claims getData(String jwt){
 
-        return Jwts.parser()
-                .setSigningKey("lepetitbidoubiwap")
-                .parseClaimsJws(jwt)
-                .getBody();
+    public Claims getData(String jwt) {
+        try {
+            return Jwts.parser()
+                    .setSigningKey("lepetitbidoubiwap")
+                    .parseClaimsJws(jwt)
+                    .getBody();
+        } catch (Exception e) {
+            // Une erreur s'est produite pendant le parsing du JWT, le token est donc invalide
+            return null;
+        }
     }
 
     public boolean isTokenValid(String jwt){
         Claims données = getData(jwt);
-        return true;
+        return données != null;  // Si les données sont null, le token n'est pas valide
     }
+
 }
